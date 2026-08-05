14a561000a581d1510e756d2ca24d157bfce274c
Anirudh valluri <anirudhvalluri07@gmail.com>
2026-08-05 10:38:29 +0530

    Time: 1043 ms (23.21%) | Memory: 8.3 MB (88.34%) - LeetSync

---
+++ 507-perfect-number/perfect-number.c
@@ -0,0 +1,14 @@
+#include<math.h>
+bool checkPerfectNumber(int num) {
+    int s=0;
+    for(int i=1;i<=num/2;i++)
+    {
+        if(num%i==0)
+        {
+            s=s+i;
+        }
+
+    }
+    return(s==num);
+    
+}
